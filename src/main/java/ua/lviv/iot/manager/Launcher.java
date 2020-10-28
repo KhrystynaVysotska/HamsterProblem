c937ba5690e0a8da5cbd6ec3ffc908e414570c66
KhrystynaVysotska <khrystynavysotska@gmail.com>
2020-10-28 02:26:52 +0200

    Modified main class to write output to file

--- src/main/java/ua/lviv/iot/manager/Launcher.java
+++ src/main/java/ua/lviv/iot/manager/Launcher.java
@@ -1,14 +1,16 @@
 package ua.lviv.iot.manager;
 
+import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 import ua.lviv.iot.model.Hamster;
 import ua.lviv.iot.seller.Seller;
 
 public class Launcher {
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) throws IOException {
 		String fileName = args[0];
 		Scanner scanner = new Scanner(new File(fileName));
 		int numberOfFoodPackagesAvailable = scanner.nextInt();
@@ -19,7 +21,10 @@ public class Launcher {
 				hamsters[counter] = new Hamster(Integer.parseInt(scanner.next()), Integer.parseInt(scanner.next()), numberOfHamstersInShop - 1);
 			}
 		}
-		System.out.println(Seller.findMaxNumberOfHamsters(numberOfFoodPackagesAvailable, hamsters));
-		System.out.println(Seller.counter);
+		int maxMaxNumberOfHamsters = Seller.findMaxNumberOfHamsters(numberOfFoodPackagesAvailable, hamsters); 
+		String outputFileName = "D:\\AlgorithmLabs\\SecondLab\\hamsterAlgorithm\\hamstersOutput.txt";
+		BufferedWriter writer = new BufferedWriter(new FileWriter(outputFileName));
+		writer.write(maxMaxNumberOfHamsters + "\ncounter: " + Seller.counter);
+		writer.close();
 	}
 }
