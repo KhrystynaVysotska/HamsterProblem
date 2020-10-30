e786265e559dc850311270a7dd9858319eac69cc
KhrystynaVysotska <khrystynavysotska@gmail.com>
2020-10-30 21:28:17 +0200

    Modified Hamster class: removed unnecessary field and modified method which returns total food consumption

--- src/main/java/ua/lviv/iot/model/Hamster.java
+++ src/main/java/ua/lviv/iot/model/Hamster.java
@@ -4,11 +4,10 @@ public class Hamster {
 
 	private int dailyNorm;
 	private int greedinessPerOneHamster;
-	private int totalFoodConsuming;
-	public Hamster(int dailyNorm, int greedinessPerOneHamster, int numberOfOtherHamsters) {
+	
+	public Hamster(int dailyNorm, int greedinessPerOneHamster) {
 		this.dailyNorm = dailyNorm;
 		this.greedinessPerOneHamster = greedinessPerOneHamster;
-		setTotalFoodConsuming(numberOfOtherHamsters);
 	}
 	public int getDailyNorm() {
 		return dailyNorm;
@@ -16,10 +15,7 @@ public class Hamster {
 	public int getGreedinessPerOneHamster() {
 		return greedinessPerOneHamster;
 	}
-	public int getTotalFoodConsuming() {
-		return totalFoodConsuming;
-	}
-	public void setTotalFoodConsuming(int numberOfOtherHamsters) {
-		this.totalFoodConsuming = dailyNorm + greedinessPerOneHamster * numberOfOtherHamsters;
+	public int getTotalFoodConsuming(int numberOfOtherHamsters) {
+		return dailyNorm + greedinessPerOneHamster * numberOfOtherHamsters;
 	}
 }
