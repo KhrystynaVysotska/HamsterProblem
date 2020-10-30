38e2befd0f8a43bc1c1be957342390d962784e4d
KhrystynaVysotska <khrystynavysotska@gmail.com>
2020-10-30 21:25:13 +0200

    Modified launcher class to work with new algorithm and take output file name as argument

--- src/main/java/ua/lviv/iot/manager/Launcher.java
+++ src/main/java/ua/lviv/iot/manager/Launcher.java
@@ -7,25 +7,25 @@ import java.io.IOException;
 import java.util.Scanner;
 
 import ua.lviv.iot.model.Hamster;
-import ua.lviv.iot.seller.Seller;
-import ua.lviv.iot.seller.SellerVersion2;
+import ua.lviv.iot.seller.SellerVersion3;
 
 public class Launcher {
 	public static void main(String[] args) throws IOException {
-		String fileName = args[0];
-		Scanner scanner = new Scanner(new File(fileName));
+		String inputFileName = args[0];
+		String outputFileName = args[1];
+		Scanner scanner = new Scanner(new File(inputFileName));
 		int numberOfFoodPackagesAvailable = scanner.nextInt();
 		int numberOfHamstersInShop = scanner.nextInt();
 		Hamster[] hamsters = new Hamster[numberOfHamstersInShop];
 		for(int counter = 0; counter < numberOfHamstersInShop; counter++) {
 			if(scanner.hasNext()) {
-				hamsters[counter] = new Hamster(Integer.parseInt(scanner.next()), Integer.parseInt(scanner.next()), 0);
+				hamsters[counter] = new Hamster(Integer.parseInt(scanner.next()), Integer.parseInt(scanner.next()));
 			}
 		}
-		int maxMaxNumberOfHamsters = SellerVersion2.findMaxNumberOfHamsters(numberOfFoodPackagesAvailable, hamsters); 
-		String outputFileName = "D:\\AlgorithmLabs\\SecondLab\\hamsterAlgorithm\\hamstersOutput.txt";
+		SellerVersion3 seller = new SellerVersion3(hamsters, numberOfFoodPackagesAvailable);
+		int maxMaxNumberOfHamsters =seller.findMaxNumberOfHamsters(); 
 		BufferedWriter writer = new BufferedWriter(new FileWriter(outputFileName));
-		writer.write(maxMaxNumberOfHamsters + "\ncounter: " + Seller.counter);
+		writer.write(maxMaxNumberOfHamsters+"");
 		writer.close();
 	}
 }
