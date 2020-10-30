f85573584f784a1d24e8b5009a78e3a58cb13ca9
KhrystynaVysotska <khrystynavysotska@gmail.com>
2020-10-30 21:24:10 +0200

    Modified some existing tests and added new for SellerVersion3

--- src/test/java/ua/lviv/iot/seller/SellerTest.java
+++ src/test/java/ua/lviv/iot/seller/SellerTest.java
@@ -1,46 +1,81 @@
 package ua.lviv.iot.seller;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
-import org.junit.Before;
 import org.junit.Test;
 
 import ua.lviv.iot.model.Hamster;
 
 public class SellerTest {
-	private Hamster[] firstCaseHamsters = new Hamster[3];
-	private Hamster[] secondCaseHamsters = new Hamster[4];
-	private Hamster[] thirdCaseHamsters = new Hamster[2];
-	
-	@Before
-	public void initTestArray() {
-		firstCaseHamsters[0] = new Hamster(1, 2, 0);
-		firstCaseHamsters[1] =  new Hamster(2, 2, 0);
-		firstCaseHamsters[2] =  new Hamster(3, 1, 0);
-		
-		secondCaseHamsters[0] = new Hamster(5, 0, 0);
-		secondCaseHamsters[1] = new Hamster(2, 2, 0);
-		secondCaseHamsters[2] = new Hamster(1, 4, 0);
-		secondCaseHamsters[3] = new Hamster(5, 1, 0);
-		
-		thirdCaseHamsters[0] = new Hamster(1, 50000, 0);
-		thirdCaseHamsters[1] = new Hamster(1, 60000, 0);
-	}
-	
 	@Test
 	public void testFirstCaseHamsters() {
-		int result = SellerVersion2.findMaxNumberOfHamsters(7, firstCaseHamsters);
+		Hamster[] firstCaseHamsters = {new Hamster(200, 0),new Hamster(1, 0),new Hamster(1, 100), new Hamster(400, 0)};
+		SellerVersion3 seller = new SellerVersion3(firstCaseHamsters, 150);
+		int result = seller.findMaxNumberOfHamsters();
 		assertEquals(2, result);
 	}
 	
 	@Test
 	public void testSecondCaseHamsters() {
-		int result = SellerVersion2.findMaxNumberOfHamsters(19, secondCaseHamsters);
+		Hamster[] secondCaseHamsters = {new Hamster(10, 0),new Hamster(2, 6),new Hamster(1, 0), new Hamster(1, 0)};
+		SellerVersion3 seller = new SellerVersion3(secondCaseHamsters, 12);
+		int result = seller.findMaxNumberOfHamsters();
 		assertEquals(3, result);
 	}
 	@Test
 	public void testThirdCaseHamsters() {
-		int result = SellerVersion2.findMaxNumberOfHamsters(2, thirdCaseHamsters);
+		Hamster[] firstCaseHamsters = {new Hamster(1, 2),new Hamster(2, 2),new Hamster(3, 1)};
+		SellerVersion3 seller = new SellerVersion3(firstCaseHamsters, 7);
+		int result = seller.findMaxNumberOfHamsters();
+		assertEquals(2, result);
+	}
+	@Test
+	public void testForthCaseHamsters() {
+		Hamster[] forthCaseHamsters = {new Hamster(5, 0),new Hamster(2, 2),new Hamster(1, 4),new Hamster(5, 1)};
+		SellerVersion3 seller = new SellerVersion3(forthCaseHamsters, 19);
+		int result = seller.findMaxNumberOfHamsters();
+		assertEquals(3, result);
+	}
+	@Test
+	public void testFifthCaseHamsters() {
+		Hamster[] fifthCaseHamsters = {new Hamster(1, 50000),new Hamster(1, 60000)};
+		SellerVersion3 seller = new SellerVersion3(fifthCaseHamsters, 2);
+		int result = seller.findMaxNumberOfHamsters();
 		assertEquals(1, result);
 	}
+	@Test
+	  public void testSixthCaseHamsters() {
+	    Hamster[] sixthCaseHamsters = {new Hamster(1, 5),new Hamster(2, 4),new Hamster(3, 3), new Hamster(4, 2), new Hamster(5, 1)};
+	    SellerVersion3 seller = new SellerVersion3(sixthCaseHamsters, 12);
+	    int result = seller.findMaxNumberOfHamsters();
+	    assertEquals(2, result);
+	  }
+	@Test
+	  public void testSeventhCaseHamsters() {
+	    Hamster[] seventhCaseHamsters = {new Hamster(100, 1),new Hamster(1, 100),new Hamster(101, 0), new Hamster(0, 101)};
+	    SellerVersion3 seller = new SellerVersion3(seventhCaseHamsters, 50);
+	    int result = seller.findMaxNumberOfHamsters();
+	    assertEquals(1, result);
+	  }
+	@Test
+	  public void testEighthCaseHamsters() {
+	    Hamster[] eighthCaseHamsters = {new Hamster(100, 1),new Hamster(1, 100),new Hamster(101, 0), new Hamster(0, 101)};
+	    SellerVersion3 seller = new SellerVersion3(eighthCaseHamsters, 100);
+	    int result = seller.findMaxNumberOfHamsters();
+	    assertEquals(1, result);
+	  }
+	@Test
+	  public void testNinthCaseHamsters() {
+	    Hamster[] ninthCaseHamsters = {new Hamster(100, 1),new Hamster(1, 100),new Hamster(101, 0), new Hamster(0, 101)};
+	    SellerVersion3 seller = new SellerVersion3(ninthCaseHamsters, 202);
+	    int result = seller.findMaxNumberOfHamsters();
+	    assertEquals(2, result);
+	  }
+	@Test
+	  public void testTenthCaseHamsters() {
+	    Hamster[] tenthCaseHamsters = {new Hamster(0, 0),new Hamster(0, 1),new Hamster(1, 0)};
+	    SellerVersion3 seller = new SellerVersion3(tenthCaseHamsters, 3);
+	    int result = seller.findMaxNumberOfHamsters();
+	    assertEquals(3, result);
+	  }
 }
